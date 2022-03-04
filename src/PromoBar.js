2b5dcf17f086a1a6727a36e4b783a956184e4bd3
Jett <premiumconsulting@jettdembin.dev>
2022-03-04 09:01:39 -0500

    promobar revision on lineheight, color, and letter spacing

---
+++ src/PromoBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./PromoBar.css"
+
+function PromoBar() {
+  return (
+    <div className="PromoBar">
+      <h1 className="PromoBar-Info">FREE SHIPPING ON ALL ORDERS</h1>
+    </div>
+  )
+}
+
+export default PromoBar;
---
+++ src/mui/index.js
@@ -0,0 +1,13 @@
+export { default as Add } from  '@mui/icons-material/Add';
+
+export { default as Dehaze } from '@mui/icons-material/Dehaze';
+
+export { default as DeleteForeverIcon } from "@mui/icons-material/DeleteForever";
+
+export { default as EditIcon } from "@mui/icons-material/Edit";
+
+export { default as PersonIcon } from "@mui/icons-material/Person";
+
+export { default as Search } from "@mui/icons-material/Search";
+
+export { default as ShoppingCartIcon } from "@mui/icons-material/ShoppingCart";
