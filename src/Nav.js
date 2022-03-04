bf9e622903d313e5ff90ae95aa13ba781084a75b
Jett <premiumconsulting@jettdembin.dev>
2022-03-04 08:46:06 -0500

    nav for mobile complete

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,31 @@
 import React from "react";
+import "./Nav.css";
+import * as mui from "./mui";
 
 function Nav() {
   return (
     <div className="Nav">
-    Nav
+      <div className="Nav-left">
+        <div className="Nav-left-navigation">
+          <mui.Dehaze aria-label="toggle-menu"/>
+        </div>
+        <div className="Nav-left-navigation">
+          <mui.Add  role="presentation" />
+        </div>
+      </div>
+      <div>
+        <div className="Nav-right">
+          <div className="Nav-right-navigation">
+            <mui.Search  aria-label="search" />
+          </div>
+          <div className="Nav-right-navigation">
+            <mui.PersonIcon aria-label="account"/>
+          </div>
+          <div className="Nav-right-navigation">
+            <mui.ShoppingCartIcon  aria-label="cart" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
