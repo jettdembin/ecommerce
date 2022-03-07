19e9aa76cff18a02261519d75b78f3c77ffd75f9
Jett <premiumconsulting@jettdembin.dev>
2022-03-07 10:30:45 -0500

    commit for menu toggle before working on animation

--- src/LinkCategory.js
+++ src/LinkCategory.js
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LinkCategory.css";
 
 import * as mui from "./mui";
 
 function LinkCategory(props) {
+  const [isExpanded, setMenu] = useState(false);
+  const toggleMenu = () => {
+    setMenu(!isExpanded);
+  };
   return (
     <div className="LinkCategory">
       <div className="LinkCategory-header-cntr">
-        <h1 className="LinkCategory-header">{props.linkCategory}</h1>
-        <mui.RemoveIcon className="LinkCategory-icon" />
+        {isExpanded ? (
+          <>
+            <div className="LinkCategory-header-wrapper">
+              <h1 className="LinkCategory-header">{props.linkCategory}</h1>
+              <mui.RemoveIcon
+                className="LinkCategory-icon"
+                onClick={toggleMenu}
+              />
+            </div>
+            <div>
+              <ul className="LinkCategory-link-cntr">
+                <li className="LinkCategory-link">Accessibility</li>
+                <li className="LinkCategory-link">Contact Us</li>
+                <li className="LinkCategory-link">Return Policy</li>
+                <li className="LinkCategory-link">FAQ</li>
+                <li className="LinkCategory-link">Gift Certificates</li>
+                <li className="LinkCategory-link">Wishlist</li>
+              </ul>
+            </div>
+          </>
+        ) : (
+          <div className="LinkCategory-header-wrapper">
+            <h1 className="LinkCategory-header">{props.linkCategory}</h1>
+            <mui.Add className="LinkCategory-icon" onClick={toggleMenu} />
+          </div>
+        )}
       </div>
-      <ul className="LinkCategory-link-cntr">
-        <li className="LinkCategory-link">Accessibility</li>
-        <li className="LinkCategory-link">Contact Us</li>
-        <li className="LinkCategory-link">Return Policy</li>
-        <li className="LinkCategory-link">FAQ</li>
-        <li className="LinkCategory-link">Gift Certificates</li>
-        <li className="LinkCategory-link">Wishlist</li>
-      </ul>
     </div>
   );
 }
