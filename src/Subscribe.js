1f6264f9d0c3b64576a6fb48398719a1ebda2f0d
Jett <premiumconsulting@jettdembin.dev>
2022-03-08 08:35:20 -0500

    finished subscribe component and fixed ul width size from 800px to 600px

--- src/Subscribe.js
+++ src/Subscribe.js
@@ -1,21 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Subscribe.css";
 import { EmailIcon } from "./mui/index";
 
+import { useMediaQuery } from "react-responsive";
+
 function Subscribe() {
+  const [val, setVal] = useState("");
+  const handleChange = (e) => {
+    setVal(e.target.value);
+  };
+  const reset = () => {
+    setVal("");
+  };
+
+
+  const checkIfMediumPlus = useMediaQuery({
+    query: "(min-width: 650px)",
+  });
+
   return (
-    <div className="Subscribe">
-      <div>
-        <h2 className="Subscribe-header">
-          <EmailIcon />
-          Sign Up and Stay Connected
-        </h2>
-        <p className="Subscribe-info">
-          Sign up for the newsletter and get 20% off! Gain access to exclusive
-          offers and be the first to know when new stuff drops!
-        </p>
-      </div>
-      <button className="Subscribe-btn">SUBSCRIBE</button>
+    <div className="Subscribe-desktop">
+      {checkIfMediumPlus ? 
+        <div className="Subscribe-cntr-desktop">
+            <h2 className="Subscribe-header">
+              <EmailIcon style={{width:"100px", fontSize:"60px"}}/>
+              Sign Up and Stay Connected
+            </h2>
+            <div className="Subscribe-info-cntr">
+              <p className="Subscribe-info-desktop">
+                Sign up for the newsletter and get 20% off! Gain access to exclusive
+                offers and be the first to know when new stuff drops!
+              </p>
+              <form onSubmit={(e)=> {
+                e.preventDefault();
+                reset();
+              }}>
+                <input type="text" value={val} placeholder="Enter Your Email Address" onChange={handleChange} />
+              </form>
+              <button className="Subscribe-btn">SUBSCRIBE</button>
+            </div>
+        </div>
+      :
+      <>
+        <div>
+          <h2 className="Subscribe-header">
+            <EmailIcon />
+            Sign Up and Stay Connected
+          </h2>
+          <p className="Subscribe-info">
+            Sign up for the newsletter and get 20% off! Gain access to exclusive
+            offers and be the first to know when new stuff drops!
+          </p>
+        </div>
+        <button className="Subscribe-btn">SUBSCRIBE</button>
+      </>
+      }
     </div>
   );
 }
